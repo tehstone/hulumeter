22bec68bd3867edb66439b003d4d2305231fa223
Richard Peng <richard.peng@hulu.com>
2012-03-15 15:30:32 -0700

    assets, ignore swap

--- requests.py
+++ requests.py
@@ -1,6 +1,20 @@
-from tornado.web import RequestHandler
+import os
+import cfg
 
-class HelloRequest(RequestHandler):
+from tornado.web import RequestHandler
+from tornado import template
+
+class TemplateRequest(RequestHandler):
+
+    _template_path = os.path.join(cfg.root, 'templates')
+    _loader = template.Loader(_template_path)
+
+    def render_template(self, template, **kwargs):
+        template = self._loader.load(template)
+        rendered = template.generate(**kwargs)
+        self.write(rendered)
+
+class HelloRequest(TemplateRequest):
     def get(self):
-        self.write("Hello world!")
+        self.render_template('base.html')
 
